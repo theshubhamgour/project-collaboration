6aec2dfbdf204fe0eef8bac3082e078cad19e0c0
Shubham Gour <iamtheshubhamgour@gmail.com>
2024-06-11 20:57:52 +0530

    Add: Solution to problem statement

---
+++ Python/Primitive-Datatypes/Basic/4_conversion.py
@@ -0,0 +1,9 @@
+# 4. **Write a Python code to convert the integer `42` to a string.**
+
+num = 42
+# Before conversion
+print(type(num))
+
+#After Conversion
+converted_num = str(num)
+print(type(converted_num))
