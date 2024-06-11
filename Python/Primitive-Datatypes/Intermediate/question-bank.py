d43d590604703f5c248ecd164cd362ca75afd8f1
Shubham Gour <iamtheshubhamgour@gmail.com>
2024-06-11 20:42:15 +0530

    Add: Question Bank

---
+++ Python/Primitive-Datatypes/Intermediate/question-bank.py
@@ -0,0 +1,11 @@
+#### Intermediate
+# 1. **Write a function to check if a variable is an instance of a specific data type (e.g., integer, float).**
+# 2. **Create a function to convert a string to uppercase without using the `upper()` method.**
+# 3. **Write a Python code to parse a string and extract all digits as an integer.**
+# 4. **Create a function to convert a float to an integer without using the `int()` function.**
+# 5. **Write a function that checks if a string can be converted to a number (integer or float).**
+# 6. **Create a function to format a number to two decimal places without using built-in functions.**
+# 7. **Write a Python function to reverse a string without using slicing.**
+# 8. **Create a function to find the length of a string without using the `len()` function.**
+# 9. **Write a code snippet to convert a list of strings to a single concatenated string.**
+# 10. **Create a function to check if a string contains only alphabetic characters.**
