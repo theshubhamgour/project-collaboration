e01d30b6d23dbdf0455f25496144b128fa74391d
Shubham Gour <iamtheshubhamgour@gmail.com>
2024-06-11 21:00:27 +0530

    Add: Solution to problem statement

---
+++ Python/Primitive-Datatypes/Basic/5_floating.py
@@ -0,0 +1,7 @@
+# 5. **How do you create a floating-point number in Python? Give an example.**
+
+number = 14
+print("The type of this data type is : ",type(number))
+
+float_num = float(number)
+print("The type of this data type is : ",type(float_num))
