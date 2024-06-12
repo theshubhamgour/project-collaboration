f1af329f459c12f6ec6f9edd89cf90f6e2812bea
Shubham Gour <iamtheshubhamgour@gmail.com>
2024-06-12 23:39:49 +0530

    Add: Solution to problem statement

--- Python/Primitive-Datatypes/Basic/7_check-float.py
+++ Python/Primitive-Datatypes/Basic/7_check-float.py
@@ -1,6 +1,11 @@
 # 7. **Write a Python code to check if a number is of type float.**
+ 
+num = 14.0
+print("The type of number is : ",type(num))
 
 
+num2 = 14
+print("The type of number is : ",type(num2))
 
 
 
