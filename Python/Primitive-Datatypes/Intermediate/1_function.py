76f068fd7b932214e3de18bf73c8cfb8fd91a2d6
Shubham Gour <iamtheshubhamgour@gmail.com>
2024-06-17 21:35:50 +0530

    Add: Solution to problem statement

---
+++ Python/Primitive-Datatypes/Intermediate/1_function.py
@@ -0,0 +1,13 @@
+# Write a function to check if a variable is an instance of a specific data type (e.g., integer, float)
+
+def func(a):
+    print("The Datatype is: ",type(a))
+
+# Calling Function for Integer Datatype
+func(5)
+
+# Calling Function for Float Datatype
+func(5.6)
+
+# Calling Function for String Datatype
+func("Hola Amigos")
